798f4170a52ce937b26b91ba83a79eb9fa87b668
vinicius pereira <viniciuslink877@gmail.com>
2022-05-14 17:09:44 -0300

    aula 14.05

---
+++ javascript/1.js
@@ -0,0 +1,99 @@
+//alert("Seja bem vindo!!!");
+/*document.write("Bem vindo ao javasript");
+prompt("digite seu nome");
+resp=confirm("Será que vai chover?");
+alert("Um retorno de carro\ bem no meio desta linha");
+alert("\"Isto não saiu compo deveria\"disse ela");
+alert("esta linha tem tabulação\taqui");*/
+
+/*var a,b;
+a=5;
+b="5 Kg de açucar";
+document.write("O valor de variavel A é:"+a);
+document.write("<p>"+"O valor de variavel B é:"+b+"<p>");
+document.write("<p>"+"O valor de variavel A dividido por 2 é:"+b/2+"</p>");
+document.write("<p>"+"O valor de variavel B dividido por 2 é:"+b/2+"</p>");
+
+var a,inteiro,flutuante;
+a="45.78 Kg de açucar";
+inteiro=parseInt(a);
+flutuante=parseFloat(a);
+document.write("O valor de variavel A é:"+a);
+document.write("<p>"+"O valor numèrico inteiro da variavel é:"+inteiro+"</p>");
+document.write("<p>"+"O valor da variável A dividido por 2 é:"+inteiro/2+"</p>");
+document.write("<p>"+"O valor da variável decimal da variável é:"+flutuante+"</p>");
+
+var a,b,c;
+a="Olá pessoal!";
+b="sejam bem vindos ao JavaScript";
+c=a+b;
+alert(c);*/
+
+/*a=50/2;
+b=100*5;
+c=25*2;
+d=50-50;
+e=50%2;
+
+document.write(a);
+document.write(b);
+document.write(c);
+document.write(d);
+document.write(e);*/
+
+/*hoje=new Date();
+xDia=hoje.getDay();
+pratoDia=new Array(7);
+diaSemana=new Array(7);
+preçoPrato=new Array(7);
+
+//dia da semana
+
+diaSemana[0]="Domingo";
+diaSemana[1]="Segunda-feira";
+diaSemana[2]="Terça-feira";
+diaSemana[3]="Quarta-feira";
+diaSemana[4]="Quinta-feira";
+diaSemana[5]="Sexta-feira";
+diaSemana[6]="Sábado";
+
+//dados da matriz do prato do dia
+
+pratoDia[0]="Lasanha";
+pratoDia[1]="Frango ao molho";
+pratoDia[2]="Arroz a grega";
+pratoDia[3]="Feijoada";
+pratoDia[4]="Nhoque";
+pratoDia[5]="Bacalhau";
+pratoDia[6]="Feijão branco";
+
+//preço do prato
+
+preçoPrato[0]="R$ 12,20";
+preçoPrato[1]="R$ 10,00";
+preçoPrato[2]="R$ 9,40";
+preçoPrato[3]="R$ 16,70";
+preçoPrato[4]="R$ 13,20";
+preçoPrato[5]="R$ 21,70";
+preçoPrato[6]="R$ 18,80";
+
+document.write("O número da semana é:"+" "+(xDia+1));
+document.write("<p><b>"+"Hoje é: "+diaSemana[xDia]);
+document.write("<p><b>"+"Nossa sugestão de hoje: "+pratoDia[xDia]);
+document.write("<p><b>"+"Nosso preço: "+preçoPrato[xDia]);*/
+
+for(i=1;i<5;i++){
+
+}
+
+valor=new Array(3,2,4,1,5);
+i=0;
+total=0;
+while(total<10){
+    total+=valor[i];i++;
+}
+document.write("Total="+total);   //Resultado:Total=10
+
+
+
+
